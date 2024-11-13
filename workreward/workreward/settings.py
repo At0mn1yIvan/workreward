1a8245507763d143eade18e9c9b72e446f90d224
At0mn1yIvan <khramov.vanya@bk.ru>
2024-11-13 19:15:20 +0300

    enhancement: replace the database with postgreSQL. Additional files: - data.json - file for storing initial data from an empty db. - db.sqlite3 - file with the previous db. It will be deleted later.

--- workreward/workreward/settings.py
+++ workreward/workreward/settings.py
@@ -15,8 +15,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -78,11 +79,14 @@ WSGI_APPLICATION = "workreward.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-# TODO: поменять СУБД на postgreSQL
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get('DB_NAME'),
+        "USER": os.environ.get('DB_USER'),
+        "PASSWORD": os.environ.get('DB_PASS'),
+        "HOST": os.environ.get('DB_HOST', 'localhost'),
+        "PORT": "5432",
     }
 }
 
